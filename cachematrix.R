15b00288d01d97b3a6249be2b1ab9be4fb7b3971
Alice Sandmeyer <asandmeyer@physik.uni-bielefeld.de>
2020-07-06 20:31:00 +0200

    Edit of file so that an inversed matrix in a cache wont be calculated twice

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL # create i as empty object
+    set <- function(y) { # intitialize storage of x and i
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x # put x in "get"
+    setinverse <- function(inverse) i <<- inverse # define storage for i
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    #see wether inverse has been calculated or not
+    if(!is.null(i)){
+        message("getting cache data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data) # inverse matrix
+    x$setinverse(i) # store inverse in cache
+    i
 }
